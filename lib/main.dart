c5f0aad4838df07c6faca5698c8653b795e62f75
Alex <awangardowie@gmail.com>
2019-08-23 16:22:46 +0200

    0.1 - Demo Screen

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-       
         primarySwatch: Colors.blue,
       ),
-     // home: ,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Petdoption screen'),
+          backgroundColor: Colors.deepPurple,
+        ),
+        body: Text('Demo Text'),
+      ),
     );
   }
 }
-
-
